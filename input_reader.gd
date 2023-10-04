62d9183fe136a6214763734175f0af62dbcd0e79
pfanne <pfanne@pm.me>
2023-10-04 11:38:06 +0200

    Initial commit

---
+++ input_reader.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var button_prefix:String = "p1_"
+@onready var input_buffer:Array[String] = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+#	input_types:Array(String) = 
+	pass
